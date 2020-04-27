4f1ea97ed89f89cb018998315e6ec93938723ffa
fabricio3g <48396639+fabricio3g@users.noreply.github.com>
2020-04-26 23:43:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+window.addEventListener('keydown', playSound )
+
+function playSound(e){
+   
+    const audio = document.querySelector( `audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+    
+    key.id = 'playing' ;
+   
+    if(!audio){
+        return;
+    }
+    audio.currentTime = 0;
+    audio.play(e);
+   e.preventDefault();
+}
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform' ){
+        return;
+    }
+    this.id = 'playing'.remove;
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
